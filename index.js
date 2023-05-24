1dfa01bbe7f081fad0cf199f2f5b2224193bcd7c
Axle <bloodymountain@outlook.com>
2023-05-24 19:27:33 -0400

    test push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ process.on("uncaughtException", (error) => console.log(error));
 const app = require("./src/Application.js");
 
 ("use strict");
-
+console.log(`eee`)
 app
   .register()
   .then(() => {
